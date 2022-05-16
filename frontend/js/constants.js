4df389dbe49556e5b3fc7c7a96f7810696d5b829
Roomfeel <grijalva_alfredo@hotmail.com>
2022-05-15 20:42:42 -0500

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x617A240d94D73675418bf1bD1EdAC3E6875e468F"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
