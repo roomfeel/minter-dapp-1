146ccf04fcd5945023d52a1bbc30e1231894d397
Roomfeel <grijalva_alfredo@hotmail.com>
2022-05-19 23:54:30 -0500

    uri

--- backend/src/config.js
+++ backend/src/config.js
@@ -69,7 +69,7 @@ const PUBLIC_MINT_START_DATE = "2022-05-18T11:30:48+00:00"; // This is required.
 const PRESALE_MINT_START_DATE = "2022-05-17T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xEE0BCbA846377f848c0Fe5d591f3D57f0d73a0d9"; // Address that will receive the royalty
-const BASE_URI = null; // only update if you want to manually set the base uri
+const BASE_URI = "ipfs://bafybeifue5za2z3ni7g75tzhskvjffh5tdkrg7st3iqbglxlfjvzgybteq/"; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = ["0xEE0BCbA846377f848c0Fe5d591f3D57f0d73a0d9"]; // only update if you want to manually set the whitelisted addresses
 
